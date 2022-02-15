89ac20b229f063fca502706612a483fe8b367e34
IgnacioChalub <ignaciochalub@gmail.com>
2022-02-15 15:24:37 -0300

    errase unused folder

--- src/contexts/shared/aplication/messagers/messager.ts
+++ src/contexts/shared/aplication/messagers/messager.ts
@@ -3,7 +3,7 @@ import IHandler from "../handlers/handler";
 interface Messager{
     handlers: IHandler[];
     sendMessage(event: any): void;
-    addHandler(hanlder: IHandler): void;
+    addHandler(handler: IHandler): void;
 }
 
 export default Messager;
